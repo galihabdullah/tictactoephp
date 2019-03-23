50ba72efc876a798197f8b46b16ea4c103eaa6f6
galih abdullah <galihabdullah471@gmail.com>
2019-03-23 20:05:26 +0700

    a

--- app/Controllers/AbstractController.php
+++ app/Controllers/AbstractController.php
@@ -8,7 +8,7 @@ use Slim\Views\Twig as TwigViews;
  *
  * @SWG\Swagger(
  *     basePath="",
- *     host="localhost:9000",
+ *     host="https://galihtictactoephp.herokuapp.com",
  *     schemes={"http"},
  *     @SWG\Info(
  *         version="1.0.0.0",
